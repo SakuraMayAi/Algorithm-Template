59a447288e69f2112032368f31efcf3f20784390
SakuraMayAi <45293640+SakuraMayAi@users.noreply.github.com>
2024-07-30 14:24:14 +0800

    Update KMP.h

--- String/KMP/KMP.h
+++ String/KMP/KMP.h
@@ -1,7 +1,7 @@
 /**
  * @brief Find and return the index of the first occurrence of s1 in s2, or -1 if s1 is not part of s2.
  * 
- * @param s1	Text string.
+ * @param s1	Main string.
  * @param s2    Pattern string.
  * @return The index of the first occurrence of s1 in s2, or -1 if s1 isn't in s2.
  */
