13a10423dd019a0cae196d04ecc509f6fa635f59
SakuraMayAi <45293640+SakuraMayAi@users.noreply.github.com>
2024-07-30 12:03:25 +0800

    Delete String/KMP.h

--- String/KMP.h
+++
@@ -1,8 +0,0 @@
-/**
- * @brief Find and return the index of the first occurrence of s1 in s2, or -1 if s1 is not part of s2.
- * 
- * @param s1	Text string.
- * @param s2    Pattern string.
- * @return The index of the first occurrence of s1 in s2, or -1 if s1 isn't in s2.
- */
-int KMP(const string& s1, const string& s2);
