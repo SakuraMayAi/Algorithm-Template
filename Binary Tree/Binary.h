01ad354715d49557318dd02b1543692d63e69403
SakuraMayAi <45293640+SakuraMayAi@users.noreply.github.com>
2024-08-03 19:42:15 +0800

    Create Binary.h

---
+++ Binary Tree/Binary.h
@@ -0,0 +1,8 @@
+typedef struct TreeNode
+{
+    int val;
+    TreeNode* left;
+    TreeNode* right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+} Node;
