66565148e81b3da470c240397f0fd3df079eeb32
SakuraMayAi <45293640+SakuraMayAi@users.noreply.github.com>
2024-08-08 11:31:55 +0800

    Delete Binary Tree/Binary.h

--- Binary Tree/Binary.h
+++
@@ -1,8 +0,0 @@
-typedef struct TreeNode
-{
-    int val;
-    TreeNode* left;
-    TreeNode* right;
-    TreeNode() : val(0), left(nullptr), right(nullptr) {}
-    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
-} Node;
